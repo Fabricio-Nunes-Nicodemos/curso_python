5a314bd2e45184077b0551bab623627f1a230610
Fabricio-Nunes-Nicodemos <fabricio.nicodemos@outlook.com>
2024-12-02 12:45:13 -0300

    Fazendo exercícios de funções

---
+++ 04_section_intermediaria/exercicios/01_funcoes.py
@@ -0,0 +1,23 @@
+# Exercícios com funções
+
+# Crie uma função que multiplica todos os argumentos não nomeados recebidos;
+# Retorne o total para uma variável e mostre o valor da variável.
+
+def multiplicacao(*args):
+    total = 1
+    for numeros in args:
+        total *= numeros
+    return total
+
+print(multiplicacao(1, 2, 3, 4, 5))
+print(1*2*3*4*5)
+
+# Crie uma função que fala se um número é par ou ímpar. Retorne se o número é par ou ímpar.
+
+def par_ou_impar(numero):
+    if numero % 2 == 0:
+        return f'O número {numero} é PAR'
+    else:
+        return f'O número {numero} é IMPAR'
+    
+print(par_ou_impar(12313223))
