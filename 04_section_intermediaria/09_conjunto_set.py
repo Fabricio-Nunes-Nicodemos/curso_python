b4e51b768c5a9baa7b0c13c4a3fbccef0fc3d774
Fabricio-Nunes-Nicodemos <fabricio.nicodemos@outlook.com>
2024-12-03 12:40:59 -0300

    Vendo funções lambda

--- 04_section_intermediaria/09_conjunto_set.py
+++ 04_section_intermediaria/09_conjunto_set.py
@@ -8,8 +8,8 @@
 # Criando um set
 # set(iterável) ou {1, 2, 3}
 # s1 = set('Luiz')
-s1 = set()  # vazio
-s1 = {'Luiz', 1, 2, 3}  # com dados
+# s1 = set()  # vazio
+# s1 = {'Luiz', 1, 2, 3}  # com dados
 
 # Sets são eficientes para remover valores duplicados
 # de iteráveis.
@@ -18,12 +18,33 @@ s1 = {'Luiz', 1, 2, 3}  # com dados
 # - não tem índexes;
 # - não garantem ordem;
 # - são iteráveis (for, in, not in)
+# l1 = [1, 2, 3, 3, 3, 3, 3, 3, 1]
+# s1 = set(l1)
+# l2 = list(s1)
+# s1 = set('Luiz')
+# print(s1)
 
 # Métodos úteis:
 # add, update, clear, discard
+s1 = set()
+s1.add('luiz')
+s1.add(1)
+s1.update(('Olá mundo', 1, 2, 3, 4))
+# s1.clear()
+s1.discard('Olá mundo')
+s1.discard('luiz')
+# print(s1)
+
 
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s1 - s2
+s3 = s1 ^ s2
+print(s3)
