36def5d08d3a1facfcb5c7211069f4528f4f682a
Fabricio-Nunes-Nicodemos <fabricio.nicodemos@outlook.com>
2024-12-03 13:49:28 -0300

    Vendo a funcao Generator

---
+++ 04_section_intermediaria/12_empacotamento_desempacotamento.py
@@ -0,0 +1,45 @@
+a, b = 1, 2
+a, b = b, a
+# print(a, b)
+
+
+# (a1, a2), (b1, b2) = pessoa.items()
+# print(a1, a2)
+# print(b2, b2)
+
+# for chave, valor in pessoa.items():
+#     print(chave, valor)
+
+pessoa = {
+    'nome': 'Aline',
+    'sobrenome': 'Souza',
+}
+
+dados_pessoa = {
+    'idade': 16,
+    'altura': 1.6,
+}
+
+pessoa_completa = {**pessoa, **dados_pessoa}
+
+# print(pessoa, dados_pessoa)
+# print()
+# print(pessoa_completa)
+
+def mostro_argumentos_nomeados(*args, **kwargs):
+    print('NÃO NOMEADOS: ', args)
+
+    for chave, valor in kwargs.items():
+        print(chave, valor)
+
+# mostro_argumentos_nomeados(nome='Joana', qlq=123)
+# mostro_argumentos_nomeados(**pessoa_completa)
+
+configuracoes = {
+    'arg1': 1,
+    'arg2': 2,
+    'arg3': 3,
+    'arg4': 4,
+}
+
+mostro_argumentos_nomeados(**configuracoes)
